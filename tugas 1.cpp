d7e790195a9b88425c56e3c66a6eaf9d3ec782ab
HwDxBIMA <47941792+HwDxBIMA@users.noreply.github.com>
2019-03-02 22:05:33 +0700

    Update tugas 1.cpp

--- tugas 1.cpp
+++ tugas 1.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 
 int main(){
-	int angka;
-	cout << "masukan angka : "; 
-	cin >> angka;
-	if(angka%2==0)
-		cout << "angka genap";
-	else
-		cout << "angka ganjil";
+	cout << "Hello World";
 	return 0;	
 }
